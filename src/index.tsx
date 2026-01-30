37dbf4cdcd21573ee88986859ca1bd1ff668bf89
xiaoheshi <shixiaohe3112@163.com>
2026-01-30 10:15:30 +0800

    feat: add smooth scroll provider with Lenis + GSAP

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
 import App from './App';
+import { SmoothScrollProvider } from './components/providers/SmoothScrollProvider';
+import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -13,7 +14,9 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <SmoothScrollProvider>
+        <App />
+      </SmoothScrollProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
